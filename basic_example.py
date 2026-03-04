da6141b91ffb07e5d8969a5fb699136141880c26
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-03-05 01:08:00 +0530

    basic_example.py

--- basic_example.py
+++ basic_example.py
@@ -1,12 +1,17 @@
 import jax.numpy as jnp
+from jax import grad, jit, vmap
 
 def predict(params, inputs):
     for w, b in params:
         outputs = jnp.dot(inputs, w) + b
-        outputs = jnp.tanh(outputs)
-    return outputs
+        inputs = jnp.tanh(outputs)
+    return inputs
 
 def loss(params, batch):
     inputs, targets = batch
     predictions = predict(params, inputs)
-    return jnp.sum((predictions - targets) ** 2)
+    return jnp.mean((predictions - targets) ** 2)
+
+
+gradient_function = jit(grad(loss))
+perexample_grads = jit(vmap(grad(loss), in_axes=(None, 0)))
