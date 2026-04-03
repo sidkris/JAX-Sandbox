f749c98fcf22e9c4538768935a1ef617d79decc9
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-03 22:12:22 +0530

    mini_llm

--- 20M LLM/mini_llm.py
+++ 20M LLM/mini_llm.py
@@ -52,3 +52,25 @@ class TransformerBlock(nnx.Module):
         return x
     
 
+class MiniLLM(nnx.Module):
+
+    def __init__(self, maxlen, vocab_size, embed_dim, num_heads, feed_forward_dim, num_transformer_blocks, *, rngs):
+        self.maxlen = maxlen 
+        self.embedding = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim, rngs = rngs)
+        self.transformer_blocks = [TransformerBlock(embed_dim, num_heads, feed_forward_dim, rngs = rngs) for _ in range(num_transformer_blocks)]
+        self.output_layer = nnx.Linear(embed_dim, vocab_size, use_bias = False, rngs = rngs)
+
+    def causal_attention_mask(self, seq_len):
+        return jnp.tril(jnp.ones((seq_len, seq_len)))
+    
+    def __call__(self, token_ids):
+        seq_len = token_ids.shape[1]
+        mask = self.causal_attention_mask(seq_len)
+        x = self.embedding(token_ids)
+        
+        for block in self.transformer_blocks:
+            x = block(x, mask = mask)
+        
+        logits = self.output_layer(x)
+        return logits
+    
