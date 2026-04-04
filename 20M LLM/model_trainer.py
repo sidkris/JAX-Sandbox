ec472e1b2d2dd392f21fccf96486266b0d824af7
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-04 22:25:12 +0530

    model training

--- 20M LLM/model_trainer.py
+++ 20M LLM/model_trainer.py
@@ -13,7 +13,15 @@ num_epochs = 3
 text_dl, batches_per_epoch = load_data()
 total_steps = num_epochs * batches_per_epoch
 
-model = MiniLLM()
+model = MiniLLM(
+    maxlen = 128,
+    vocab_size = tokenizer.n_vocab,
+    embed_dim = 192,
+    num_heads = 6,
+    feed_forward_dim = 512, 
+    num_transformer_blocks = 6,
+    rngs = nnx.Rngs(0)
+)
 
 def loss_fn(model, batch):
     inputs, targets = batch
@@ -42,3 +50,39 @@ optimizer = nnx.Optimizer(
 metrics = nnx.MultiMetric(
     loss = nnx.metrics.Average("loss")
 )
+
+
+@nnx.jit
+def train_step(model, optimizer, metrics, batch):
+    grad_fn = nnx.value_and_grad(loss_fn, has_aux = True)
+    (loss, logits), grads = grad_fn(model, batch)
+
+    metrics.update(loss, logits, labels = batch[1])
+    optimizer.update(grads)
+
+
+metrics_history = {"train_loss" : []}
+
+
+prep_target_batch = jax.vmap(
+    lambda tokens : jnp.concatenate((tokens[1:], jnp.array([0])))
+)
+
+
+for epoch in range(num_epochs):
+    step = 0
+    for batch in text_dl:
+        input_batch = jnp.array(jnp.array(batch).T).astype(jnp.int32)
+        target_batch = prep_target_batch(jnp.array(jnp.array(batch).T)).astype(jnp.int32)
+        print(".", end = "")
+        train_step(model, optimizer, metrics, (input_batch, target_batch))
+
+        if (step + 1) % 2 == 0:
+            for metric, value in metrics.compute().items():
+                metrics_history[f"train_{metric}"].append(value)
+            
+            metrics.reset()
+
+            current_lr = lr_schedule(step)
+            print(f"\EPOCH : {epoch + 1} | STEP : {step + 1} | LOSS : {metrics_history} | LR : {current_lr}\n")
+        step += 1
