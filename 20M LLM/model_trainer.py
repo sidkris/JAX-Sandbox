cd256c28ed56aae84409b3e7574d3110557d8f47
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-04-04 22:11:13 +0530

    model training

--- 20M LLM/model_trainer.py
+++ 20M LLM/model_trainer.py
@@ -9,8 +9,9 @@ from mini_llm import MiniLLM
 
 maxlen = 128
 tokenizer = tiktoken.get_encoding("gpt2")
-
+num_epochs = 3
 text_dl, batches_per_epoch = load_data()
+total_steps = num_epochs * batches_per_epoch
 
 model = MiniLLM()
 
@@ -19,3 +20,25 @@ def loss_fn(model, batch):
     logits = model(inputs)
     loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets).mean()
     return loss, logits
+
+
+warmup_steps = max(1, total_steps // 10)
+print(f"Total Training Steps : {total_steps:,}")
+print(f"Warmup Steps : {warmup_steps:,}")
+
+lr_schedule = optax.warmup_cosine_decay_schedule(
+    init_value = 0.0,
+    peak_value = 3e-4,
+    warmup_steps = warmup_steps,
+    decay_steps = total_steps,
+    end_value = 1e-5
+)
+
+optimizer = nnx.Optimizer(
+    model, 
+    optax.adamw(learning_rate = lr_schedule, weight_decay = 0.01)
+)
+
+metrics = nnx.MultiMetric(
+    loss = nnx.metrics.Average("loss")
+)
